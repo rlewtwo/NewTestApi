22728b1a9b61a72ea9a6d4d6ffeb003644ac7737
Nextspace <nextspace@ITSadmins-MacBook-Air.local>
2017-12-21 18:56:30 -0800

    server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/db');
 const app            = express();
-const port = 9011;
+const port = 9012;
 app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
